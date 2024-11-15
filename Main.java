68bc04a21ae0b6ffd52902735f7daa6e067ab8ef
Munaga Sai Ram <168403985+rammunaga@users.noreply.github.com>
2024-11-15 12:53:11 +0530

    This is 5th commit to Main branch and class Main

--- Main.java
+++ Main.java
@@ -10,6 +10,8 @@ public class Main {
        List<Integer> lst=List.of(1,2,3,4,5,6,7,8,9);
        long count=lst.stream().filter(PrimeChecker::isprime).count();
        System.out.println(count);
+       System.out.println(lst);
 
+        System.out.println("This is for checking intellj");
     }
 }
